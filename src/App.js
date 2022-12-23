1c8c10b27824ab233331ebaa00f23186cab7aea5
nel386 <nel386@gmail.com>
2022-12-23 20:20:31 +0100

    firstDay

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import NavBar from "./components/layout/navigation/NavBar";
-import HourGlass from "./components/UI/Spinners/HourGlass";
-const TasksManager = lazy(() => import("./views/TasksManager"));
-const About = lazy(() => import("./views/About"));
-const LoginPage = lazy(() => import("./views/Login/LoginPage"))
-// import TasksManager from "./views/TasksManager";
-// import About from "./views/About";
-// import LoginPage from "./views/Login/LoginPage";
-
+import classes from "./App.module.css";
 
 function App() {
   return (
     <>
-      <header>
-        <h1>Deploy</h1>
-        <NavBar></NavBar>
-      </header>
-      <Suspense fallback={
-        <div class="full-size flex-center">
-          <HourGlass />
-        </div>
-      }>
-        <Routes>
-          <Route path="/" element={<TasksManager />}></Route>
-          <Route path="/deploy1" element={<TasksManager />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
-        </Routes>
-      </Suspense>
+      <div className={classes.btn}>
+        <span ></span>
+        <span ></span>
+      </div>
     </>
   );
 }
