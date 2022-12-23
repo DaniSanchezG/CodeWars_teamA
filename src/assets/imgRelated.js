0394df608942973eaf296f3221e9be652192c9db
Nacho Martin <nachomg93@gmail.com>
2022-12-23 20:09:01 +0100

    Iniciando el project

---
+++ src/assets/imgRelated.js
@@ -0,0 +1,10 @@
+import arvel from "./Img/Related/arvel.jpg"
+import cassian from "./Img/Related/cassian.jpg"
+import arvel from "./Img/Related/arvel.jpg"
+import arvel from "./Img/Related/arvel.jpg"
+import arvel from "./Img/Related/arvel.jpg"
+import arvel from "./Img/Related/arvel.jpg"
+
+export default {
+
+}
