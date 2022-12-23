386c93446e834b7e340a771ba8ef3a673c59b7a5
Daniel <dvallejosanchez@gmail.com>
2022-12-23 20:21:26 +0100

    practica

---
+++ src/components/Interactive/Interactive.js
@@ -0,0 +1,41 @@
+import classes from "./Interactive.module.css";
+
+function Interactive() {
+  return (
+    <div className={classes.container}>
+      <h2 className={classes.title}>The Latest | Star Wars Games + Apps</h2>
+      <div className={classes.line}></div>
+      <div className={classes["cards-container"]}>
+      <div className={classes["wrapper-img1"]}>
+        <img className={classes.img} src={require("../../assets/1.jpeg")} />
+        <h3 className={classes["text-1"]}>
+          Star Wars: The Clone Wars Invades the Tabletop
+        </h3>
+      </div>
+      <ul className={classes.lateral}>
+        <li>
+          <div className={classes["wrapper-img2"]}>
+          <img  src={require("../../assets/2.webp")} />
+          <h3 className={classes["text-2"]}>Star Wars Jedi: Survivor: Cameron Monaghan on Cal Kestis' Next Adventure</h3>
+          </div>
+        </li>
+        <li>
+        <div className={classes["wrapper-img2"]}>
+          <img  src={require("../../assets/3.webp")} />
+          <h3 className={classes["text-2"]}>Star Wars Jedi: Survivor: Cameron Monaghan on Cal Kestis' Next Adventure</h3>
+          </div>
+        </li>
+        <li>
+        <div className={classes["wrapper-img2"]}>
+          <img  src={require("../../assets/4.webp")} />
+          <h3 className={classes["text-2"]}>Star Wars Jedi: Survivor: Cameron Monaghan on Cal Kestis' Next Adventure</h3>
+          </div>
+        </li>
+      </ul>
+      </div>
+    </div>
+    
+  );
+}
+
+export default Interactive;
