bc19cfb02f1dd69ddd792e255e7f08f8d0f00058
JuanDomínguez <juandominortega92@gmail.com>
2022-12-27 19:01:48 +0100

    Final footer

---
+++ src/components/UI/Buttons/ButtonNeon.js
@@ -0,0 +1,14 @@
+import classes from "./ButtonNeon.module.css";
+
+function ButtonNeon() {
+  return (
+    <>
+      <div className={classes["-container"]}>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  );
+}
+
+export default ButtonNeon;
