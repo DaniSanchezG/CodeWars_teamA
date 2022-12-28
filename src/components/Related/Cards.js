38c7dccd5ad64ecce5e47b5c99c518fe5128280b
nacho <nachomg93@gmail.com>
2022-12-28 19:05:55 +0100

    Intentar eliminar el componente Button con una condicional

--- src/components/Related/Cards.js
+++ src/components/Related/Cards.js
@@ -4,20 +4,34 @@ import classes from "./Cards.module.css";
 //Components
 import ButtonNeon from "../UI/Buttons/ButtonNeon";
 
+//Datas
+import { datas } from "../../db/datas";
+
 function Cards(props) {
   // console.log(props);
   return (
     <>
-      <section className={classes.related}>
-        <div className={classes["cards-container"]}>
-          <img src={props.datas[0].img.path} alt={props.datas[0].img.text} />
-          <div className={classes["characters-container"]}>
-            <ButtonNeon />
-            <div className={classes["content-info"]}>
-              <h3>{props.datas[0].character}</h3>
-            </div>
-          </div>
-        </div>
+        <section className={classes.related}>
+          <ul>
+      {datas.map((data) => {
+        return (
+            <li key={data.id} character={data.character} img={data.img}>
+                <div className={classes["cards-container"]}>
+                  <img
+                    src={data.img.path}
+                    alt={data.img.text}
+                  />
+                  <div className={classes["characters-container"]}>
+                    <ButtonNeon />
+                    <div className={classes["content-info"]}>
+                      <h3>{data.character}</h3>
+                    </div>
+                  </div>
+                </div>
+            </li>
+        );
+      })}
+          </ul>
       </section>
     </>
   );
--- src/components/Related/Related.js
+++ src/components/Related/Related.js
@@ -1,15 +1,12 @@
 //CSS
 import classes from "./Related.module.css";
 
-//Hooks
-import { useState } from "react";
-
 function Related() {
   return (
     <>
       <div className={classes.container}>
         <div className={classes.title}>
-          <h2>Related</h2>
+          <p>Related</p>
         </div>
       </div>
     </>
