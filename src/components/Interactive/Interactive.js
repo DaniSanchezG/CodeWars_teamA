230fd7ca5f108e239bb3b6092218d5946e8fb816
Daniel <dvallejosanchez@gmail.com>
2022-12-28 18:56:33 +0100

    miercoles

--- src/components/Interactive/Interactive.js
+++ src/components/Interactive/Interactive.js
@@ -11,30 +11,28 @@ function Interactive() {
         <h3 className={classes["text-1"]}>
           Star Wars: The Clone Wars Invades the Tabletop
         </h3>
-        <div className={classes.trapecio1}></div>
+        {/* <div className={classes.trapecio1}></div> */}
       </div>
       <ul className={classes.lateral}>
-        <li>
+       
           <div className={classes["wrapper-img2"]}>
-          <img  src={require("../../assets/2.webp")} />
+          <img className={classes.img2} src={require("../../assets/2.webp")} />
           <h3 className={classes["text-2"]}>Star Wars Jedi: Survivor: Cameron Monaghan on Cal Kestis' Next Adventure</h3>
           </div>
-          <div className={classes.trapecio}></div>
-        </li> 
-        <li>
+         <img className={classes.trapecio} src={require("../../assets/trapecio.jpg")}  />
+       
         <div className={classes["wrapper-img2"]}>
-          <img  src={require("../../assets/3.webp")} />
-          <h3 className={classes["text-2"]}>Star Wars Jedi: Survivor: Cameron Monaghan on Cal Kestis' Next Adventure</h3>
+          <img className={classes.img2} src={require("../../assets/3.webp")} />
+          <h3 className={classes["text-2"]}>Cal Kestis Returns in Star Wars Jedi: Battle Scars - Cover Reveal</h3>
           </div>
-          <div className={classes.trapecio}></div>
-        </li>
-        <li>
+          {/* <div className={classes.trapecio}></div> */}
+        
         <div className={classes["wrapper-img2"]}>
-          <img  src={require("../../assets/4.webp")} />
-          <h3 className={classes["text-2"]}>Star Wars Jedi: Survivor: Cameron Monaghan on Cal Kestis' Next Adventure</h3>
+          <img className={classes.img2} src={require("../../assets/4.webp")} />
+          <h3 className={classes["text-2"]}>Quiz: Which Star Wars Video Game Should You Play Right Now?</h3>
           </div>
-          <div className={classes.trapecio}></div>
-        </li>
+          {/* <div className={classes.trapecio}></div> */}
+        
       </ul>
       </div>
     </div>
